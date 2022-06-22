4a0bc9b177322e4375af0188bfee4024b1c68a2c
German <Makeev_German@mail.ru@example.com>
2022-06-22 22:27:32 +0300

    tas19

--- task19/Program.cs
+++ task19/Program.cs
@@ -4,3 +4,26 @@
 // 23432 -> да
 // 12821 -> да
 Console.WriteLine ("Введите число пятизначное");
+// int [] через массив решить
+int number  =  Convert.ToInt32(Console.ReadLine());
+int numberA = number/10000;
+int numberE = number%10;
+
+
+// Console.WriteLine ($"{numberA} + {numberE}");
+if( numberA == numberE);
+
+// else{
+//     Console.WriteLine("нет");
+//     }
+int numberB = number/1000 % 10;
+int numberD = number/10%10;
+if( numberB == numberD )
+{Console.WriteLine("да");
+}
+// then {Console.WriteLine("да")};
+else{
+    Console.WriteLine("нет");
+    }
+
+//  Console.WriteLine ($"{numberD}");

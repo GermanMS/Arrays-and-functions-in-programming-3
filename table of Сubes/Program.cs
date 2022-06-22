4d688ac13b861ea194c47257bebda83d654a6184
German <Makeev_German@mail.ru@example.com>
2022-06-22 21:01:09 +0300

    Task 21 is readyd

--- table of Сubes/Program.cs
+++ table of Сubes/Program.cs
@@ -1,4 +1,10 @@
-﻿Console.WriteLine ("Введите число");
-int number = Convert.ToInt32(Console.ReadLine());
+﻿// *Задача 23: * Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 4, 9.
+// 5 -> 1, 8, 27, 64, 125
 
+Console.WriteLine ("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= number; i++){
+    Console.WriteLine(i*i*i);
+}
 
